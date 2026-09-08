b1d8119af0303b4f648c9117af61388fae3d6020
Taffy4396 <1711432087@qq.com>
2026-09-08 13:29:13 +0800

    Delete restrunant_sql directory

--- restrunant_sql/修改3.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `修改3` (`step`, `rows_cnt`) VALUES ('1.5 清洗后总行数', 44512);
--- restrunant_sql/地理位置分析.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `地理位置分析` (`business_district`, `均价`, `中位数`, `店铺数`) VALUES ('燕莎/朝阳公园商业区', 268, 160, 323);
-INSERT INTO `地理位置分析` (`business_district`, `均价`, `中位数`, `店铺数`) VALUES ('国贸地区', 204, 128, 720);
-INSERT INTO `地理位置分析` (`business_district`, `均价`, `中位数`, `店铺数`) VALUES ('望京/酒仙桥/798地区', 144, 114, 423);
-INSERT INTO `地理位置分析` (`business_district`, `均价`, `中位数`, `店铺数`) VALUES ('三里屯/工体/东直门地区', 152, 105, 934);
-INSERT INTO `地理位置分析` (`business_district`, `均价`, `中位数`, `店铺数`) VALUES ('劲松/潘家园地区', 90, 89, 463);
