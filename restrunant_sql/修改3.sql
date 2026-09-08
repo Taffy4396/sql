1315f01418f2c85ddaf2459d2aa705b5394bcca3
Taffy4396 <1711432087@qq.com>
2026-09-08 13:20:26 +0800

    Add files via upload

---
+++ restrunant_sql/修改3.sql
@@ -0,0 +1,1 @@
+INSERT INTO `修改3` (`step`, `rows_cnt`) VALUES ('1.5 清洗后总行数', 44512);
---
+++ restrunant_sql/错误数据.sql
@@ -0,0 +1,4 @@
+INSERT INTO `错误数据` (`缺失数`, `有值数`, `缺失率`) VALUES (27063, 17449, 60.8);
+INSERT INTO `错误数据` (`SUM(avg_price=0)`, `ROUND(100.0*SUM(avg_price=0)/COUNT(*),1)`) VALUES (0, 0.0);
+INSERT INTO `错误数据` (`COUNT(DISTINCT dist_desc)`, `COUNT(*)-COUNT(dist_desc)`) VALUES (586, 8867);
+INSERT INTO `错误数据` (`缺失数`, `有值数`, `缺失率`) VALUES (44090, 422, 99.1);
